da6a3f2a71222f3c5d8e9651fdf6c897a6b2b3ae
Randal Grant <randal.grant@gmail.com>
2013-06-02 03:38:31 +1000

    now fully explores edges

--- src/view/gui/WorldMap.java
+++ src/view/gui/WorldMap.java
@@ -106,7 +106,11 @@ public class WorldMap extends JPanel {
 				} else {
 					piece.myType = chr;
 				}
-				piece.u = 0; // unused agent.w.u[yy][xx];
+				if (agent.hasNeighboursUnexplored(xx, yy)) {
+					piece.u = 1;
+				} else {
+					piece.u = 0;
+				}
 				//System.out.print(piece.myType);
 				piece.update();
 			}
